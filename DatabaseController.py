db4465838b420209324700942791149864e5da21
Gustavo <gustavolemos7@hotmail.com>
2021-04-16 11:05:28 -0300

    Feat: database methods structure

---
+++ DatabaseController.py
@@ -0,0 +1,30 @@
+class Users:
+    def insert_user(self, fullname, birth, cpf, height):
+        pass
+
+    def update_user_by_id(self, fullname, birth, cpf, height):
+        pass
+
+    def update_user_by_cpf(self, fullname, birth, cpf, height):
+        pass
+
+    def update_all(self, fullname, birth, cpf, height):
+        pass
+
+    def get_users(self):
+        pass
+
+    def get_user_by_id(self):
+        pass
+
+    def get_user_by_cpf(self):
+        pass
+
+    def delete_user_by_id(self):
+        pass
+
+    def delete_user_by_cpf(self):
+        pass
+
+    def delete_all(self):
+        pass
